574a3707be065ec80cf052daec1fc1dfce6dcd1a
Danito Anastasova <101755968+DanitoAnastasova@users.noreply.github.com>
2025-03-18 23:34:20 +0200

    Add files via upload

---
+++ Repositories/IOrderRepository.cs
@@ -0,0 +1,13 @@
+﻿using BookStore.Models;
+
+namespace BookStore.Repositories
+{
+    public interface IOrderRepository
+    {
+        Task<IEnumerable<Order>> GetAllOrdersAsync();
+        Task<Order> GetOrderByIdAsync(int id);
+        Task AddOrderAsync(Order order);
+        Task UpdateOrderAsync(Order order);
+        Task DeleteOrderAsync(int id);
+    }
+}
